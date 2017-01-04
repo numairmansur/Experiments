53fdebbb8a2296c00ab2199808f3c7bcb2d53d54
numair <numair.mansur@gmail.com>
2017-01-04 21:53:05 +0100

    cccc

--- forrester/forrester19/forrester.py
+++ forrester/forrester19/forrester.py
@@ -3,18 +3,17 @@ import sys
 import math
 import time
 import csv
-from hpolib.benchmarks.synthetic_functions import Bohachevsky
-from hpolib.benchmarks.synthetic_functions import 
+from hpolib.benchmarks.synthetic_functions import Forrester
 from time import gmtime, strftime
 
 def main(job_id, params):
   print '!!! Entered Main !!!'
   print 'Anything printed here will end up in the output directory for job #:', str(job_id)
   print params
-  b = Bohachevsky()
-  res = b.objective_function([params['x'], params['y']])
+  f = Forrester()
+  res = f.objective_function([params['x'], params['y']])
   print res
-  with open('/home/numair/Music/Experiments/Bohachevsky/run1.csv','a') as csvfile:
+  with open('/home/mansurm/Experiments/forrester/run19.csv','a') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     writer.writerow([res['main'][0]])
   return res['main'][0]
