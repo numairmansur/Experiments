4b361cebe1ec5128dcfe6824d1fc7d65f07878d3
numair <numair.mansur@gmail.com>
2017-01-18 15:59:38 +0100

    Forrester update

--- forrester/forrester22/forrester.py
+++ forrester/forrester22/forrester.py
@@ -11,8 +11,9 @@ def main(job_id, params):
   print 'Anything printed here will end up in the output directory for job #:', str(job_id)
   print params
   f = Forrester()
-  res = f.objective_function([params['x'], params['y']])
+  res = f.objective_function([params['x']])
   print res
+  #with open('/home/numair/Downloads/testEnvFortheCluster/Experiments/forrester/run1.csv','a') as csvfile:
   with open('/home/mansurm/Experiments/forrester/run22.csv','a') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     writer.writerow([res['main'][0]])
--- forrester/forrester5/config.pb
+++ forrester/forrester5/config.pb
@@ -5,14 +5,6 @@ variable {
  name: "x"
  type: FLOAT
  size: 1
- min:  -5
- max:  10
-}
-
-variable {
- name: "y"
- type: FLOAT
- size: 1
  min:  0
- max:  15
+ max:  1
 }
